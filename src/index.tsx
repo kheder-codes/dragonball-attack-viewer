66accb8293d4092e2ba467010fd3e5345b65fc27
Marlen Kheder <kheder@me.com>
2025-05-06 17:14:07 +0200

    fixed basename

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/dragonball-attack-viewer">
       <App />
     </BrowserRouter>
   </React.StrictMode>
