cc9177efef1e2824428fe8d729e0b4f7000d7e2a
bzharkrk <alankerkuki@googlemail.com>
2025-05-19 12:46:22 +0200

    Stiländerungen für AttackItem-Komponente und CSS-Variablen hinzugefügt

--- src/components/AttackItem.tsx
+++ src/components/AttackItem.tsx
@@ -8,7 +8,22 @@ interface AttackItemProps {
 
 const AttackItem: React.FC<AttackItemProps> = ({ attack }) => {
   return (
-    <li className="bg-white border-[3px] border-[#404853] rounded-lg p-4 shadow-md m-2 hover:scale-110 transition-transform duration-200 text-center list-none max-w-[200px]">
+    <li className="
+      bg-gradient-to-b
+      from-mittelgrau
+      to-hellblau
+      rounded-lg
+      p-4
+      shadow-md
+      m-2
+      hover:scale-110
+      transition-transform
+      duration-200
+      text-white
+      text-center
+      list-none
+      max-w-[200px]
+    ">
       <Link to={`/attacks/${attack.attackId}`} className="text-inherit no-underline flex flex-col items-center">
         <div className="w-full h-40">
           <img
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust if your component files are elsewhere
-    "./public/index.html"
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mittelgrau: 'var(--mittelgrau)',
+        hellblau: 'var(--hellblau)',
+      },
+    },
   },
   plugins: [],
 }
-
